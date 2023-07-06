7fa53c991ae9f3677d09e3cc66e4a3ee65bb2508
Anthony Lines <linesanthony018@gmail.com>
2023-07-06 17:01:51 +0200

    MAP

--- src/components/TaskCreator.jsx
+++ src/components/TaskCreator.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import useNewId from "../hooks/useNewId";
+import styled from "styled-components";
+
+const ContainerList = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 8;
+  background-color: blue;
+`;
 
 export default function TaskCreator(props) {
   const getNewId = useNewId();
-  return <div>TaskCreator</div>;
+
+  return (
+    <div>
+      <h2>Quentin est un tyran</h2>
+    </div>
+  );
 }
--- src/pages/TodoListPage.jsx
+++ src/pages/TodoListPage.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 import TaskCreator from "../components/TaskCreator";
 import TaskItem from "../components/TaskItem";
+import styled from "styled-components";
+
+const ContainerList = styled.div`
+  display: flex;
+  justify-content: center;
+
+  background-color: blue;
+`;
 
 export default function TodoListPage() {
-  return <div>TodoListPage</div>;
+  const TestList = [
+    { id: 0, name: "Faire du code" },
+    { id: 1, name: "Faire du code pour de vrai" },
+    { id: 2, name: "Faire du clean code" },
+    { id: 3, name: "Dl error lens" },
+    { id: 4, name: "Arreter de faire le gros noob" },
+  ];
+
+  return (
+    <ContainerList>
+      <h1>Liste de tâches :</h1>
+      <ul>
+        <h1>stream en pause tuteur afk</h1>
+        {TestList.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </ContainerList>
+  );
 }
