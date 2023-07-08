d449d96a769f0fdb49183369d66fb66838a14076
Anthony Lines <linesanthony018@gmail.com>
2023-07-08 20:45:51 +0200

    feat:

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import styled from "styled-components";
+
+const ListItem = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+const TaskName = styled.span`
+  flex-grow: 1;
+  margin-right: 8px;
+`;
+
+const TaskItem = (props) => {
+  const handleDelete = () => {
+    props.onDeleteTask(props.task);
+  };
+
+  const handleEdit = () => {};
+};
 
 export default function TaskItem() {
-  return <div>TaskItem</div>;
+  return (
+    <ListItem>
+      <TaskName>{props.task.name}</TaskName>
+      <button onClick></button>
+    </ListItem>
+  );
 }
