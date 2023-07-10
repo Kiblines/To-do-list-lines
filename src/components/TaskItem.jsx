0425b61f9eea188559b0d98c136026ffe66dd920
Anthony Lines <linesanthony018@gmail.com>
2023-07-10 17:16:49 +0200

    Acceptable

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -16,6 +16,36 @@ const TaskName = styled.span`
   flex-grow: 1;
   margin-right: 8px;
 `;
+const SaveButton = styled.button`
+  height: 30px;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 4px;
+  background-color: #4caf50;
+  color: #fff;
+  cursor: pointer;
+  font-size: 14px;
+  margin-right: 5px;
+`;
+
+const CancelButton = styled.button`
+  height: 30px;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 4px;
+  background-color: #fbc02d;
+  color: #fff;
+  cursor: pointer;
+  font-size: 14px;
+`;
+
+const EditInput = styled.input`
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 14px;
+  margin-right: 5px;
+`;
 
 const DeleteButton = styled.button`
   height: 30px;
@@ -28,9 +58,7 @@ const DeleteButton = styled.button`
   font-size: 14px;
   margin-right: 5px;
 `;
-const CompleteIcon = styled.span`
-  font-weight: bold;
-`;
+
 const EditButton = styled.button`
   height: 30px;
   padding: 5px 10px;
@@ -42,7 +70,7 @@ const EditButton = styled.button`
   font-size: 14px;
 `;
 const CompleteButton = styled.button`
-  background-color: #4caf50;
+  background-color: ${(props) => (props.completed ? "#4caf50" : "#ccc")};
   border: none;
   border-radius: 4px;
   cursor: pointer;
@@ -50,19 +78,19 @@ const CompleteButton = styled.button`
   font-size: 14px;
   height: 30px;
   padding: 5px 10px;
-
   margin: 8px;
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: #45a049;
+    background-color: ${(props) => (props.completed ? "#45a049" : "#ccc")};
   }
 
   &:focus {
     outline: none;
-    box-shadow: 0 0 0 2px #4caf50;
+    box-shadow: 0 0 0 2px ${(props) => (props.completed ? "#4caf50" : "#ccc")};
   }
 `;
+
 const TaskItem = (props) => {
   const [editing, setEditing] = useState(false);
   const [newTaskName, setNewTaskName] = useState(props.task.name);
@@ -95,14 +123,14 @@ const TaskItem = (props) => {
     <ListItem>
       {editing ? (
         <>
-          <input
+          <EditInput
             type="text"
             value={newTaskName}
             onChange={handleChange}
             autoFocus
           />
-          <button onClick={handleSave}>Save</button>
-          <button onClick={handleCancel}>Cancel</button>
+          <SaveButton onClick={handleSave}>Save</SaveButton>
+          <CancelButton onClick={handleCancel}>Cancel</CancelButton>
         </>
       ) : (
         <>
@@ -112,7 +140,7 @@ const TaskItem = (props) => {
               completed={props.task.isCompleted}
               onClick={() => props.onCompleteTask(props.task)}
             >
-              {props.task.isCompleted ? <CompleteIcon>✓</CompleteIcon> : ""}
+              {props.task.isCompleted ? "✓" : "X"}
             </CompleteButton>
             <DeleteButton onClick={handleDelete}>Delete</DeleteButton>
             <EditButton onClick={handleEdit}>Edit</EditButton>
